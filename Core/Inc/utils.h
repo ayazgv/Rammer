d22a18b70fb80ce0d18657fadcdda4d7d596a475
Ayaz Agayev <ayazcw@gmail.com>
2020-05-22 11:37:18 +0400

    initial commit

---
+++ Core/Inc/utils.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+void reverse(char* str, int len);
+int intToStr(int x, char str[], int d);
+void ftoa(float n, char* res, int afterpoint);
+
+#endif /* __MAIN_H */
